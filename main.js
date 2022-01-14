732d558d1a116fb7b7de6e6a118fc701621406b3
Gabriela <gabrielaauzcategui@gmail.com>
2022-01-14 15:03:42 +0100

    notificacion de envio

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 window.onload = () => {
     //capturamos el formulario
-    let form = document.getElementById("form");
+    let form = document.getElementById("email-form");
 
     //código que se ejecuta a los tres segundos de haber sido leido
     setTimeout(() => {
-        console.log('hola coders')
+        console.log("hola coders");
     }, 3000);
 
     //escuchamos el evento submit o onsubmit (pulsar el boton de enviar)
@@ -13,17 +13,20 @@ window.onload = () => {
         e.preventDefault();
 
         //capturamos los inputs de texto
-        let name = document.getElementById("inputName").value;
-        let email = document.getElementById("inputEmail").value;
-        let text = document.getElementById("inputTextarea").value;
+        let name = document.getElementById("name").value;
+        let email = document.getElementById("email").value;
+        let text = document.getElementById("field").value;
 
         //función para descargar el archivo
         function download(filename, text) {
-            var element = document.createElement('a');
-            element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-            element.setAttribute('download', filename);
+            var element = document.createElement("a");
+            element.setAttribute(
+                "href",
+                "data:text/plain;charset=utf-8," + encodeURIComponent(text)
+            );
+            element.setAttribute("download", filename);
 
-            element.style.display = 'block';
+            element.style.display = "block";
             document.body.appendChild(element);
 
             element.click();
@@ -41,48 +44,11 @@ window.onload = () => {
         download("email.txt", tempText);
 
         //ahora hay que mostrar un mensaje en la pantalla, os toca hacerlo a vosotros
-        
-        //No funcionan:
-        // function mostrarConfirmar(mensaje){
+        document.getElementById("noti").style.display = "block";
 
-        //     const confirmar = confirm(mensaje);
-        //     return confirmar.value;
-        
-        // }
-        
-        // let confirmado = mostrarConfirmar("¡Hola!, su mensaje ha sido enviado correctamente");
-
-        // -------------------
-
-        // function submitform(){
-        //     alert("Formulario enviado correctamente");
-        //     document.getElementById('form').submit();
-        //   }
-        // --------------------------------
-
-        // function enviar() {
-        //     let form;
-        //     var dato = form[0];
-        //     if (dato.value=="enviar") {
-        //       alert("Formulario enviado");
-        //       form.submit();
-        //       return true;
-        //     } else {
-        //       alert("No se envía el formulario");
-        //       return false;
-        //     }
-        //   }
-        // -------------------------
-
-        let form = document.createElement('form');
-        form.action = 'https://google.com/search';
-        form.method = 'GET';
-
-        form.innerHTML = '<input name="send" value="Enviar">';
-
-        // el formulario debe estar en el document para poder enviarlo
-        document.body.append(form);
-
-        form.submit();
-
-}
+        //para ocultar la notificacion a los 5 segundos
+        setTimeout(function() {
+            document.getElementById("noti").style.display = "none";
+        }, 5000);
+    });
+};
